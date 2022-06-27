d319b568ed1e99fa76243b14a0646585479239c8
Sergey <sss1401@inbox.ru>
2022-06-27 16:50:08 +0300

    Added project structure

---
+++ include/LinearAlgebra.hpp
@@ -0,0 +1,7 @@
+#ifndef LINEAR_ALGEBRA_HPP
+#define LINEAR_ALGEBRA_HPP
+
+#include "LinearAlgebra/Matrix.hpp"
+#include "LinearAlgebra/SolutionSLE.hpp"
+
+#endif // LINEAR_ALGEBRA_HPP
---
+++ include/LinearAlgebra/SolutionSLE.hpp
@@ -0,0 +1,8 @@
+#ifndef SOLUTION_SLE_HPP
+#define SOLUTION_SLE_HPP
+
+#include "SolutionSLE/gaussian_elimination.hpp"
+#include "SolutionSLE/inverse_matrix_method.hpp"
+#include "SolutionSLE/lu_decomposition.hpp"
+
+#endif // SOLUTION_SLE_HPP
